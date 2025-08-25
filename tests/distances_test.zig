242464ffb2b50baeb942713ea6412b5918c6fda8
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2025-08-25 18:43:17 +0300

    Removed DTW distance

--- tests/distances_test.zig
+++ tests/distances_test.zig
@@ -30,25 +30,6 @@ test "invalid input" {
     try std.testing.expectError(error.InvalidInput, distances.chebyshevDistance(&a, &b));
 }
 
-test "dtw distance" {
-    const allocator = std.testing.allocator;
-    const a = [_]f64{ 1.0, 2.0, 3.0 };
-    const b = [_]f64{ 2.0, 3.0, 4.0, 5.0 };
-    const result = try distances.dtwDistance(allocator, &a, &b, null);
-    try std.testing.expectApproxEqAbs(4, result, 1e-10);
-}
-
-test "dtw distance with window" {
-    const allocator = std.testing.allocator;
-    const a = [_]f64{ 1.0, 2.0, 3.0, 4.0, 5.0 };
-    const b = [_]f64{ 2.0, 3.0, 4.0, 5.0, 6.0 };
-
-    const result_full = try distances.dtwDistance(allocator, &a, &b, null);
-    const result_window = try distances.dtwDistance(allocator, &a, &b, 2);
-
-    try std.testing.expectApproxEqAbs(result_full, result_window, 1e-10);
-}
-
 test "lcss distance" {
     const allocator = std.testing.allocator;
     const a = [_]f64{ 1.0, 2.0, 3.0, 4.0 };
